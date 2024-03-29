70dca53d1c36be08045ff1f2ae1bafb6ea62dd6b
HARUN360 <mdharun573192@gmail.com>
2024-03-29 15:24:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Nav from "./components/Nav"
+
+
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-4xl'>Vite + React</h1>
+      <Nav></Nav>
+      
+    </>
+  )
+}
+
+export default App
